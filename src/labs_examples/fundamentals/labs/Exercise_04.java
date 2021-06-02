7fb3a7ccb0857e96dc3d86c84f0e5630890f2f8c
Melissa <melissakheying@gmail.com>
2021-06-02 12:44:35 -0500

    Complete labs in fundamentals

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,29 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
+        byte b = 100;
+        System.out.println("A byte primitive type is between -127 and 128. Variable myByte is: " + b);
 
-        // write your code below
+        short s = 4000;
+        System.out.println("A short primitive type ranges from -32,768 and 32,767. Variable myShort is: " + s);
+
+        int i = 1;
+        System.out.println("An int primitive type ranges from -2,147,483,648 to 2,147,483,647. Variable myInt is: " + i);
+
+        long l = 100000000L;
+        System.out.println("A long primitive type is the larger version of an Integer: " + l);
+
+        float f = 1.1234567f;
+        System.out.println("A float primitive type has up to 7 decimal places. Example: " + f);
+
+        double d = 3.12345678910111213;
+        System.out.println("A double primitive type is a larger version of a float. Example: " + d);
+
+        boolean isTrue = true;
+        System.out.println("Variable isTrue is: " + isTrue);
+
+        char c = 'c';
+        System.out.println("The char primitive type is a single character: " + c);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,40 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int lengthOfStr = str.length();
+        System.out.println(lengthOfStr);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean equalStrings = str.equals(str2);
+        System.out.println("Are the strings equal? " + equalStrings);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String oneString = str + " " + str2;
+        System.out.println(oneString);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+        // replace
+        System.out.println(str.replace("hello", "goodbye"));
+
+        String word = "supercalifragilisticexpialidocious";
+
+        // substring
+        String usingTheSubStringMethod = word.substring(2,3);
+        System.out.println("What is the letter between index 2 and index 3? " + usingTheSubStringMethod);
+        String anotherSubString = "This sentence is a String.";
+        System.out.println(anotherSubString.substring(4,13));
+
+        // contains
+        boolean usingTheContainsMethod = word.contains("z");
+        System.out.println("Does " + word + " contain the letter z? " + usingTheContainsMethod);
+
+        // indexOf
+        int usingTheIndexOfMethod = word.indexOf("x");
+        System.out.println("At what index is the letter 'x'? " + usingTheIndexOfMethod);
+
 
     }
 
